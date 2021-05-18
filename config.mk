3aae50be0e5426e86017b60927e6695a081b889b
Krzysztof Płaczek <krzysztof.placzek@yahoo.com>
2021-05-19 00:37:18 +0200

    Update CFLAGS

--- config.mk
+++ config.mk
@@ -27,7 +27,8 @@ LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -march=native -O3 -pipe -fno-plt -ftree-vectorize -ftree-slp-vectorize ${INCS} ${CPPFLAGS}
+#CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -march=native -O3 -pipe -fno-plt -ftree-vectorize -ftree-slp-vectorize ${INCS} ${CPPFLAGS}
+CFLAGS	= -march=native -O2 -pipe -fno-plt -fexceptions -Wp,-D_FORTIFY_SOURCE=2,-D_GLIBCXX_ASSERTIONS -Wformat -Werror=format-security -fstack-clash-protection -fcf-protection -fipa-pta -ftree-loop-distribution -floop-interchange -floop-nest-optimize -fgraphite-identity -ftree-vectorize -fsplit-paths -fpredictive-commoning -fuse-linker-plugin -flto=auto -floop-parallelize-all -ftree-parallelize-loops=4 -std=c99 -pedantic -Wall ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
 
 # Solaris
